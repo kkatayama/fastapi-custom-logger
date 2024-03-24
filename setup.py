5d775e6dcacae7fccae553676aa4370f499f63d7
Teddy Katayama <katayama@udel.edu>
2024-03-23 21:12:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 
 setup(
     name="fastapi-middleware-logger",
-    version="0.0.5",
+    version="0.0.6",
     author="Paul Déchorgnat",
     author_email="paul.dechorgnat@gmail.com",
     url="https://github.com/pauldechorgnat/fastapi-custom-logger",
